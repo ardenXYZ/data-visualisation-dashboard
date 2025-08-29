d87d7a8e84dd6fbc4519ce49bfce6f5e0aaf471a
ardenXYZ <zhong.arden@gmail.com>
2025-08-29 20:06:32 +1000

    set prisma to initial nextjs app.

---
+++ prisma/migrations/20250829095302_init/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Product" (
+    "id" SERIAL NOT NULL,
+    "productId" TEXT NOT NULL,
+    "productName" TEXT NOT NULL,
+    "openingInventory" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."DailyTransaction" (
+    "id" SERIAL NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "day" INTEGER NOT NULL,
+    "type" TEXT NOT NULL,
+    "quantity" DOUBLE PRECISION NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "DailyTransaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "public"."User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_productId_key" ON "public"."Product"("productId");
+
+-- CreateIndex
+CREATE INDEX "DailyTransaction_productId_day_idx" ON "public"."DailyTransaction"("productId", "day");
+
+-- AddForeignKey
+ALTER TABLE "public"."DailyTransaction" ADD CONSTRAINT "DailyTransaction_productId_fkey" FOREIGN KEY ("productId") REFERENCES "public"."Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
